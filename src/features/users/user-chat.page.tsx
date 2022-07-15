f4166904b7bc7c4d18f94db5263b75756fd34f06
Uwem Akan <uwemakan12@gmail.com>
2022-07-15 07:49:11 +0100

    updated user features

--- src/features/users/user-chat.page.tsx
+++ src/features/users/user-chat.page.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { IconButton, Button, Link } from '@mui/material';
+import { IconButton, Button, Link, CircularProgress, Alert, AlertTitle } from '@mui/material';
 import { AttachFile, PowerSettingsNew } from '@mui/icons-material';
 import { useContext, useEffect, useRef, useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 import {
   Container,
   ChatArea,
@@ -11,23 +12,21 @@ import {
   LogoutContainer,
   MessageComponent,
   Body,
+  AppAlert,
 } from '../../components';
 import { UserAuthContext, UserChatContext } from '../../services';
 
 export const UserChatPage = () => {
   const { logout, userId } = useContext(UserAuthContext);
-  const { messages, sendMessage } = useContext(UserChatContext);
+  const { messages, sendMessage, isLoadingChat, chatError } = useContext(UserChatContext);
   const [text, setText] = useState('');
+  const [error, setError] = useState(chatError);
 
   const messagesEndRef = useRef({} as any);
 
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
-
-  useEffect(() => {
-    scrollToBottom();
-  }, [messages]);
 
   const handleSubmitMessage = async (event: any) => {
     event?.preventDefault();
@@ -37,6 +36,20 @@ export const UserChatPage = () => {
     }
   };
 
+  const handleClose = () => {
+    setError('');
+  };
+
+  useEffect(() => {
+    setError(chatError);
+  }, [chatError]);
+
+  useEffect(() => {
+    if (messages.length) {
+      scrollToBottom();
+    }
+  }, [messages]);
+
   const chats = messages.map((m) => (
     <MessageComponent key={m.id} body={m.body} sender={m.sender} createdAt={m.createdAt} />
   ));
@@ -44,41 +57,73 @@ export const UserChatPage = () => {
   return (
     <Body>
       <Container>
-        <LogoutContainer>
-          <Link href="/login" underline="none">
-            <Button
-              onClick={() => {
-                logout();
-              }}
-              startIcon={<PowerSettingsNew />}
-              variant="text"
+        <AppAlert
+          anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+          open={!!error}
+          autoHideDuration={5000}
+          onClose={handleClose}
+        >
+          {error ? (
+            <Alert
+              severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => handleClose()}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
             >
-              Logout
-            </Button>
-          </Link>
-        </LogoutContainer>
-        <ChatArea>
-          {chats}
-          <p ref={messagesEndRef} />
-        </ChatArea>
-        <TypingArea>
-          <label htmlFor="icon-button-file">
-            <FileInput accept="image/*" id="icon-button-file" type="file" />
-            <IconButton color="primary" aria-label="upload picture" component="span">
-              <AttachFile />
-            </IconButton>
-          </label>
-          <TextArea
-            fullWidth
-            size="small"
-            variant="outlined"
-            value={text}
-            onChange={(event) => setText(event.target.value)}
-            type="text"
-            placeholder="Type message..."
-            onKeyUp={handleSubmitMessage}
-          />
-        </TypingArea>
+              <AlertTitle>{chatError}</AlertTitle>
+            </Alert>
+          ) : (
+            <div />
+          )}
+        </AppAlert>
+        {isLoadingChat ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <LogoutContainer>
+              <Link href="/login" underline="none">
+                <Button
+                  onClick={() => {
+                    logout();
+                  }}
+                  startIcon={<PowerSettingsNew />}
+                  variant="text"
+                >
+                  Logout
+                </Button>
+              </Link>
+            </LogoutContainer>
+            <ChatArea>
+              {chats}
+              <p ref={messagesEndRef} />
+            </ChatArea>
+            <TypingArea>
+              <label htmlFor="icon-button-file">
+                <FileInput accept="image/*" id="icon-button-file" type="file" />
+                <IconButton color="primary" aria-label="upload picture" component="span">
+                  <AttachFile />
+                </IconButton>
+              </label>
+              <TextArea
+                fullWidth
+                size="small"
+                variant="outlined"
+                value={text}
+                onChange={(event) => setText(event.target.value)}
+                type="text"
+                placeholder="Type message..."
+                onKeyUp={handleSubmitMessage}
+              />
+            </TypingArea>
+          </>
+        )}
       </Container>
     </Body>
   );
--- src/services/chat/chat-action-types.constant.ts
+++ src/services/chat/chat-action-types.constant.ts
@@ -2,3 +2,4 @@ export const NEW_MESSAGE = 'NEW_MESSAGE';
 export const USER_DETAILS = 'USER_DETAILS';
 export const CHAT_ERROR = 'CHAT_ERROR';
 export const LOADING_CONTENT = 'LOADING_CONTENT';
+export const GET_MESSAGES = 'GET_MESSAGES';
